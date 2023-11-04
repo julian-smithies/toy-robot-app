2a9acebac8c4f6cfcb397ab63cb605570e63811e
Julian Smithies <juliansmithies@gmail.com>
2023-11-05 10:52:59 +1100

    test long sequence of commands

--- public/index.php
+++ public/index.php
@@ -6,5 +6,5 @@ use Julian\ToyRobotApp\ToyRobot\ToyRobotCommandQueue;
 $robot = new ToyRobot();
 $table = new Table(5, 5);
 $commandQueue = new ToyRobotCommandQueue($robot, $table);
-$commandQueue->importCommandsFromFile($_SERVER['DOCUMENT_ROOT'] . '/inputs/test_commands.txt');
+$commandQueue->importCommandsFromFile($_SERVER['DOCUMENT_ROOT'] . '/inputs/test_long_sequence.txt');
 $commandQueue->invokeCommands();
