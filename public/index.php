f7f08cb028a4d185f927ad772b92c41e80f71519
Julian Smithies <juliansmithies@gmail.com>
2023-11-05 07:08:39 +1100

    example of robot and command queue functionality

--- public/index.php
+++ public/index.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
 <?php
-    
-?>
+use Julian\ToyRobot\CommandQueue;
+use Julian\ToyRobot\ToyRobot;
+
+$robot = new ToyRobot();
+$commandQueue = new CommandQueue($robot);
+$commandQueue->importFromFile($_SERVER['DOCUMENT_ROOT'] . '/inputs/test_all_commands.txt');
+$commandQueue->execute();
